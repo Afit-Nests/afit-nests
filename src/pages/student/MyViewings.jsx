7626e3d470410809b20894d60e0a49633ad2a9c1
igbamshi <victorelisha2410@gmail.com>
2026-07-04 00:10:24 +0100

    build fix

--- src/pages/student/MyViewings.jsx
+++ src/pages/student/MyViewings.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-'
+import { Link } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
 import { supabase } from '../../lib/supabase'
 import MobileNav from '../../components/common/MobileNav'
